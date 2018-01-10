590bb609d5f0a6143a73c2c25218a92af522fadc
Andrew Boyer <aboyer@Andrews-iMac.fios-router.home>
2018-01-09 19:11:07 -0500

    changes to test trigger

--- www/index.php
+++ www/index.php
@@ -18,6 +18,7 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+<p>WTF?</p>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
